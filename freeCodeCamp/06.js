bb4be1dca71286aa18eb17525ac259cb06d27719
rkpbb <antiysdota@mail.ru>
2023-12-02 19:11:30 +0300

    Declare String Variables

---
+++ freeCodeCamp/06.js
@@ -0,0 +1,9 @@
+// ❓DESCRIPTION:
+
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+
+// ✔️SOLUTION:
+
+var myFirstName = 'Vitali';
+var myLastName = 'Sak';
