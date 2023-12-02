2e85cc0b1b4dee3e56f914c5218ad3d119c7da43
rkpbb <antiysdota@mail.ru>
2023-12-02 19:06:51 +0300

    Storing Values with the Assignment Operator

---
+++ freeCodeCamp/03.js
@@ -0,0 +1,14 @@
+// ❓DESCRIPTION:
+// Assign the value 7 to variable a.
+//
+// Setup
+// var a;
+//
+// Only change code below this line
+
+
+// ✔️SOLUTION:
+// Setup
+var a;
+a = 7;
+// Only change code below this line
