744bc1a144cdc9a63835fc495cd6a05f5d4e15dd
rkpbb <antiysdota@mail.ru>
2023-12-02 19:30:55 +0300

    Explore Differences Between the var and let Keywords

---
+++ freeCodeCamp/09.js
@@ -0,0 +1,11 @@
+// ❓DESCRIPTION:
+// Update the code so it only uses the let keyword
+// Do not create any new variables.
+
+var catName = "Oliver";
+var catSound = "Meow!";
+
+
+// ✔️SOLUTION:
+let catName = "Oliver";
+let catSound = "Meow!";
