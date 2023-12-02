05b93f92c71b26133b226d580436f609a6fd5c99
rkpbb <antiysdota@mail.ru>
2023-12-02 19:08:37 +0300

    Assigning the Value of One Variable to Another

---
+++ freeCodeCamp/04.js
@@ -0,0 +1,18 @@
+// ❓DESCRIPTION:
+// Assign the contents of a to variable b.
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+
+// ✔️SOLUTION:
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
