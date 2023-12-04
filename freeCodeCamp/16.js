1c2256b6231b575bd1f5054c7e6c95fbe22f61ef
rkpbb <antiysdota@mail.ru>
2023-12-04 19:42:05 +0300

    Decrement a Number with JavaScript

---
+++ freeCodeCamp/16.js
@@ -0,0 +1,14 @@
+// ❓DESCRIPTION:
+// Change the code to use the -- operator on myVar.
+
+let myVar = 11;
+
+// Only change code below this line
+myVar = myVar - 1;
+
+
+// ✔️SOLUTION:
+let myVar = 11;
+
+// Only change code below this line
+myVar = --myVar;
