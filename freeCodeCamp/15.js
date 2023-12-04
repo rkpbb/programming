d4ab0f660e5c4af533bd66bfd1af9d5d89f38e9a
rkpbb <antiysdota@mail.ru>
2023-12-04 19:40:45 +0300

    Increment a Number with JavaScript

---
+++ freeCodeCamp/15.js
@@ -0,0 +1,14 @@
+// ❓DESCRIPTION:
+// Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+
+
+// ✔️SOLUTION:
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
