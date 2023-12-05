89217f81bca47650cbf13d7286102c7c068c56e7
rkpbb <antiysdota@mail.ru>
2023-12-05 19:40:10 +0300

    Compound Assignment With Augmented Addition

---
+++ freeCodeCamp/21.js
@@ -0,0 +1,21 @@
+// ❓DESCRIPTION:
+// Convert the assignments for a, b, and c to use the += operator.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+// ✔️SOLUTION:
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
