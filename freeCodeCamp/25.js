9625664eee393289fbd5957dd48004a35db6574c
rkpbb <antiysdota@mail.ru>
2023-12-06 19:01:40 +0300

    Escaping Literal Quotes in Strings

---
+++ freeCodeCamp/25.js
@@ -0,0 +1,8 @@
+// ❓DESCRIPTION:
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+// I am a "double quoted" string inside "double quotes".
+
+const myStr = ""; // Change this line
+
+// ✔️SOLUTION:
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
