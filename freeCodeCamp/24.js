1e06fb955973b86984e2052ff3f4c6d44a29f056
rkpbb <antiysdota@mail.ru>
2023-12-06 18:56:15 +0300

    Compound Assignment With Augmented Division

---
+++ freeCodeCamp/24.js
@@ -0,0 +1,22 @@
+// ❓DESCRIPTION:
+// Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a = a / 12;
+b = b / 4;
+c = c / 11;
+
+
+// ✔️SOLUTION:
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
