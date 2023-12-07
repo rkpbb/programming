f172ac84b71e05e6b6c8363ce2af08bc58b2e803
rkpbb <antiysdota@mail.ru>
2023-12-07 18:15:17 +0300

    Concatenating Strings with Plus Operator

---
+++ freeCodeCamp/28.js
@@ -0,0 +1,9 @@
+// ❓DESCRIPTION:
+/* Build myStr from the strings This is the start. and This is the end. using the + operator.
+Be sure to include a space between the two strings.
+*/
+
+const myStr = ""; // Change this line
+
+// ✔️SOLUTION:
+const myStr = "This is the start. " + "This is the end."; // Change this line
