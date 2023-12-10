fa8e94762765eabdaf498b059ada2d68ec5bfa36
rkpbb <antiysdota@mail.ru>
2023-12-10 18:43:21 +0300

    Constructing Strings with Variables

---
+++ freeCodeCamp/30.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION:
+/* Set myName to a string equal to your name and build myStr with myName
+ between the strings My name is  and  and I am well!
+*/
+
+// Only change code below this line
+const myName = "";
+const myStr = "";
+
+// ✔️SOLUTION:
+// Only change code below this line
+const myName = "Vitali";
+const myStr = "My name is " + myName + "and I am well!";
