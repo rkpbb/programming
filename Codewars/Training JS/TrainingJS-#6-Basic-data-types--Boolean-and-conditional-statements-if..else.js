d9effe44a53bed8fce763c774f244f15c4e2f25c
rkpbb <antiysdota@mail.ru>
2023-12-17 16:44:55 +0300

    Training JS #6: Basic data types--Boolean and conditional statements if..else

---
+++ Codewars/Training JS/TrainingJS-#6-Basic-data-types--Boolean-and-conditional-statements-if..else.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION:
+/*
+Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional
+statement if...else, if val value is false (val==false or it can convert to false), should return
+a string "false", if not, return a string "true".
+function trueOrFalse(val){
+  if ()    return ?;
+  else     return ?;
+}
+*/
+
+// ✔️SOLUTION:
+function trueOrFalse(val){
+    if (val == false || !val)    return false;
+    else     return true;
+}
