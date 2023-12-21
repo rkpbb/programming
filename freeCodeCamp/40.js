2196501e4996f9a70b9965e9c1d539dcb1c18563
rkpbb <antiysdota@mail.ru>
2023-12-21 18:23:55 +0300

    Nest one Array within Another Array

---
+++ freeCodeCamp/40.js
@@ -0,0 +1,7 @@
+// ❓DESCRIPTION:
+// Create a nested array called myArray.
+// Only change code below this line
+const myArray = [];
+// ✔️SOLUTION:
+// Only change code below this line
+const myArray = [["RED", 12], ["White Sox", 11]];
