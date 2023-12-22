e0c50fe7605addcf32652313c664424e6741b0f5
rkpbb <antiysdota@mail.ru>
2023-12-22 19:43:56 +0300

    Access Array Data with Indexes

---
+++ freeCodeCamp/41.js
@@ -0,0 +1,9 @@
+// ❓DESCRIPTION:
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+const myArray = [50, 60, 70];
+
+// ✔️SOLUTION:
+const myArray = [50, 60, 70];
+console.log(myArray[0]);
+const myData = myArray[0];
