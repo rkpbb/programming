38b9b2aea33c226550dccd56377ecde74f2b98a4
rkpbb <antiysdota@mail.ru>
2023-12-23 16:15:39 +0300

    Modify Array Data With Indexes

---
+++ freeCodeCamp/42.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION:
+// Modify the data stored at index 0 of myArray to a value of 45.
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+
+
+// ✔️SOLUTION:
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+// Only change code below this line
